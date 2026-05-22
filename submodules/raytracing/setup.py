f46674c7997af76a4c313b219bed7c63d415a8c2
gregor434 <gregor.boschmann@outlook.de>
2026-05-22 15:47:49 +0200

    fix package build for RT submodule

--- submodules/raytracing/setup.py
+++ submodules/raytracing/setup.py
@@ -1,13 +1,13 @@
 import os
+import urllib.request
+import tarfile
+import re
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 _src_path = os.path.dirname(os.path.abspath(__file__))
 
-# ref: https://github.com/sxyu/sdf/blob/master/setup.py
 def find_eigen(min_ver=(3, 3, 0)):
-    """Helper to find or download the Eigen C++ library"""
-    import re, os
     try_paths = [
         '/usr/include/eigen3',
         '/usr/local/include/eigen3',
@@ -15,43 +15,44 @@ def find_eigen(min_ver=(3, 3, 0)):
         'C:/Program Files/eigen3',
         'C:/Program Files (x86)/eigen3',
     ]
-    WORLD_VER_STR = "#define EIGEN_WORLD_VERSION"
-    MAJOR_VER_STR = "#define EIGEN_MAJOR_VERSION"
-    MINOR_VER_STR = "#define EIGEN_MINOR_VERSION"
+    
+    conda_prefix = os.environ.get('CONDA_PREFIX')
+    if conda_prefix:
+        try_paths.append(os.path.join(conda_prefix, 'include', 'eigen3'))
+        try_paths.append(os.path.join(conda_prefix, 'include'))
+
+    min_ver_str = '.'.join(map(str, min_ver))
     EIGEN_WEB_URL = 'https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.bz2'
     TMP_EIGEN_FILE = 'tmp_eigen.tar.bz2'
     TMP_EIGEN_DIR = 'eigen-3.3.7'
-    min_ver_str = '.'.join(map(str, min_ver))
 
     eigen_path = None
     for path in try_paths:
         macros_path = os.path.join(path, 'Eigen/src/Core/util/Macros.h')
         if os.path.exists(macros_path):
-            macros = open(macros_path, 'r').read().split('\n')
-            world_ver, major_ver, minor_ver = None, None, None
-            for line in macros:
-                if line.startswith(WORLD_VER_STR):
-                    world_ver = int(line[len(WORLD_VER_STR):])
-                elif line.startswith(MAJOR_VER_STR):
-                    major_ver = int(line[len(MAJOR_VER_STR):])
-                elif line.startswith(MINOR_VER_STR):
-                    minor_ver = int(line[len(MAJOR_VER_STR):])
-            if not world_ver or not major_ver or not minor_ver:
-                print('Failed to parse macros file', macros_path)
+            with open(macros_path, 'r', encoding='utf-8') as f:
+                content = f.read()
+            
+            world = re.search(r'#define\s+EIGEN_WORLD_VERSION\s+(\d+)', content)
+            major = re.search(r'#define\s+EIGEN_MAJOR_VERSION\s+(\d+)', content)
+            minor = re.search(r'#define\s+EIGEN_MINOR_VERSION\s+(\d+)', content)
+            
+            if not world or not major or not minor:
+                print('Failed to parse macros file, using path anyway:', path)
+                eigen_path = path
+                break
+            
+            ver = (int(world.group(1)), int(major.group(1)), int(minor.group(1)))
+            ver_str = '.'.join(map(str, ver))
+            if ver < min_ver:
+                print('Found unsuitable Eigen version', ver_str, 'at', path, '(need >= ' + min_ver_str + ')')
             else:
-                ver = (world_ver, major_ver, minor_ver)
-                ver_str = '.'.join(map(str, ver))
-                if ver < min_ver:
-                    print('Found unsuitable Eigen version', ver_str, 'at',
-                          path, '(need >= ' + min_ver_str + ')')
-                else:
-                    print('Found Eigen version', ver_str, 'at', path)
-                    eigen_path = path
-                    break
+                print('Found Eigen version', ver_str, 'at', path)
+                eigen_path = path
+                break
 
     if eigen_path is None:
         try:
-            import urllib.request
             print("Couldn't find Eigen locally, downloading...")
             req = urllib.request.Request(
                 EIGEN_WEB_URL,
@@ -62,15 +63,15 @@ def find_eigen(min_ver=(3, 3, 0)):
                 })
 
             with urllib.request.urlopen(req) as resp,\
-                  open(TMP_EIGEN_FILE, 'wb') as file:
+                 open(TMP_EIGEN_FILE, 'wb') as file:
                 data = resp.read()
                 file.write(data)
-            import tarfile
+            
             tar = tarfile.open(TMP_EIGEN_FILE)
             tar.extractall()
             tar.close()
 
-            eigen_path = TMP_EIGEN_DIR
+            eigen_path = os.path.join(_src_path, TMP_EIGEN_DIR)
             os.remove(TMP_EIGEN_FILE)
         except:
             print('Download failed, failed to find Eigen')
@@ -83,7 +84,7 @@ def find_eigen(min_ver=(3, 3, 0)):
 nvcc_flags = [
     '-O3', '-std=c++17',
     "--expt-extended-lambda",
-	"--expt-relaxed-constexpr",
+    "--expt-relaxed-constexpr",
     '-U__CUDA_NO_HALF_OPERATORS__', '-U__CUDA_NO_HALF_CONVERSIONS__', '-U__CUDA_NO_HALF2_OPERATORS__',
 ]
 
@@ -92,7 +93,6 @@ if os.name == "posix":
 elif os.name == "nt":
     c_flags = ['/O2', '/std:c++17']
 
-    # find cl.exe
     def find_cl_path():
         import glob
         for edition in ["Enterprise", "Professional", "BuildTools", "Community"]:
@@ -100,23 +100,14 @@ elif os.name == "nt":
             if paths:
                 return paths[0]
 
-    # If cl.exe is not on path, try to find it.
     if os.system("where cl.exe >nul 2>nul") != 0:
         cl_path = find_cl_path()
         if cl_path is None:
             raise RuntimeError("Could not locate a supported Microsoft Visual C++ installation")
         os.environ["PATH"] += ";" + cl_path
 
-'''
-Usage:
-python setup.py build_ext --inplace # build extensions locally, do not install (only can be used from the parent directory)
-python setup.py install # build extensions and install (copy) to PATH.
-pip install . # ditto but better (e.g., dependency & metadata handling)
-python setup.py develop # build extensions and install (symbolic) to PATH.
-pip install -e . # ditto but better (e.g., dependency & metadata handling)
-'''
 setup(
-    name='raytracing', # package name, import this to use python API
+    name='raytracing',
     version='0.1.0',
     description='CUDA RayTracer with BVH acceleration',
     url='https://github.com/ashawkey/raytracing',
@@ -124,7 +115,7 @@ setup(
     author_email='ashawkey1999@gmail.com',
     ext_modules=[
         CUDAExtension(
-            name='_raytracing', # extension name, import this to use CUDA API
+            name='_raytracing',
             sources=[os.path.join(_src_path, 'src', f) for f in [
                 'bvh.cu',
                 'raytracer.cu',
@@ -148,7 +139,7 @@ setup(
         'trimesh',
         'opencv-python',
         'torch',
-        'numpy ',
+        'numpy',
         'tqdm',
         'matplotlib',
         'dearpygui',
